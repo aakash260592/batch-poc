82647f66d7aceb7749327f47be78403a3b40e3da
shubham <saxena.shubham@yash.com>
2024-06-12 10:28:54 +0530

    Spring Batch POC

---
+++ src/test/java/com/jcs/batch/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
